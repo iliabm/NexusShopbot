f99bf385dca4409fa30cca37f9aa5e57b9300734
iliabm <ilia.badakhshan2009@gmail.com>
2026-07-04 11:00:28 +0330

    Create languages.py

---
+++ languages.py
@@ -0,0 +1,101 @@
+TEXT = {
+
+    "fa": {
+
+        "welcome":
+        "👋 به فروشگاه Nexus خوش اومدی\n\nلطفا یکی از گزینه‌های زیر رو انتخاب کن.",
+
+        "join":
+        "🔒 برای استفاده از ربات ابتدا عضو کانال شوید.",
+
+        "joined":
+        "✅️ تایید عضویت",
+
+        "join_btn":
+        "📢 عضویت در کانال",
+
+        "menu":
+        "🏪 منوی اصلی",
+
+        "stars":
+        "⭐ خرید Telegram Stars",
+
+        "premium":
+        "💎 خرید Telegram Premium",
+
+        "pubg":
+        "🎮 خرید PUBG UC",
+
+        "cod":
+        "🎯 خرید COD CP",
+
+        "support":
+        "📞 پشتیبانی",
+
+        "back":
+        "⬅️ بازگشت",
+
+        "payment":
+        "💳 پرداخت",
+
+        "send_receipt":
+        "📸 ارسال رسید پرداخت",
+
+        "rules":
+        "📜 قوانین",
+
+        "language":
+        "🌍 انتخاب زبان"
+
+    },
+
+    "en": {
+
+        "welcome":
+        "👋 Welcome To Nexus Shop\n\nPlease choose one of the options below.",
+
+        "join":
+        "🔒 Please join our channel first.",
+
+        "joined":
+        "✅ I've Joined",
+
+        "join_btn":
+        "📢 Join Channel",
+
+        "menu":
+        "🏪 Main Menu",
+
+        "stars":
+        "⭐ Telegram Stars",
+
+        "premium":
+        "💎 Telegram Premium",
+
+        "pubg":
+        "🎮 PUBG UC",
+
+        "cod":
+        "🎯 COD CP",
+
+        "support":
+        "📞 Support",
+
+        "back":
+        "⬅️ Back",
+
+        "payment":
+        "💳 Payment",
+
+        "send_receipt":
+        "📸 Send Receipt",
+
+        "rules":
+        "📜 Rules",
+
+        "language":
+        "🌍 Language"
+
+    }
+
+}
