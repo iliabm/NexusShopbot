50178a87f31aec80ad62e4f1c9e6655721c1d620
iliabm <ilia.badakhshan2009@gmail.com>
2026-07-04 20:45:25 +0330

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,46 +1,80 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-import config
 import database
+import config
 
 
-async def admin_panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def send_order(bot, user, product):
 
-    if update.effective_user.id != config.ADMIN_ID:
-        await update.message.reply_text("❌ شما ادمین نیستید.")
-        return
+    text = f"""
+🛒 سفارش جدید
 
-    orders = database.get_orders()
+👤 نام:
+{user.full_name}
 
-    if len(orders) == 0:
-        await update.message.reply_text("📦 هنوز سفارشی ثبت نشده است.")
-        return
+🆔 آیدی:
+{user.id}
 
-    text = "📋 لیست سفارش‌ها\n\n"
+📛 یوزرنیم:
+@{user.username if user.username else "ندارد"}
 
-    for order in orders:
-
-        text += (
-            f"🆔 سفارش: {order[0]}\n"
-            f"👤 کاربر: {order[1]}\n"
-            f"🛍 محصول: {order[2]}\n"
-            f"📦 تعداد: {order[3]}\n"
-            f"📌 وضعیت: {order[4]}\n\n"
-        )
-
-    await update.message.reply_text(text)
-
-
-async def notify_admin(bot, user_id, product, quantity):
-
-    text = (
-        "🆕 سفارش جدید\n\n"
-        f"👤 User ID : {user_id}\n"
-        f"🛍 Product : {product}\n"
-        f"📦 Quantity : {quantity}"
-    )
+📦 محصول:
+{product}
+"""
 
     await bot.send_message(
         chat_id=config.ADMIN_ID,
         text=text
     )
+
+
+async def admin_panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if update.effective_user.id != config.ADMIN_ID:
+
+        await update.message.reply_text("⛔ دسترسی ندارید.")
+        return
+
+    users = database.get_user_count()
+    orders = database.get_order_count()
+
+    text = f"""
+👨‍💻 پنل مدیریت Nexus
+
+━━━━━━━━━━━━━━
+
+👥 تعداد کاربران:
+{users}
+
+📦 تعداد سفارش‌ها:
+{orders}
+
+━━━━━━━━━━━━━━
+
+برای مشاهده سفارش‌ها:
+
+/orders
+"""
+
+    await update.message.reply_text(text)
+
+
+async def orders(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if update.effective_user.id != config.ADMIN_ID:
+        return
+
+    data = database.get_orders()
+
+    if not data:
+
+        await update.message.reply_text("📭 هنوز سفارشی ثبت نشده.")
+        return
+
+    text = "📦 لیست سفارش‌ها\n\n"
+
+    for order in data:
+
+        text += (
+            f"🆔 {order[0]}\n"
+            f"
